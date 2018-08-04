c8e7aec0d70ad40a731c29362f16dde2afdb7faf
stainlessrat <41853144+stainlessrat@users.noreply.github.com>
2018-08-04 20:14:00 +0700

    sin угла

---
+++ 01_DOM/home_work_08/js/script.js
@@ -0,0 +1,9 @@
+//const form = document.querySelector('form');
+//const form = document.forms[0].elements.quantility; - добраться до элемента по атребуту имени имени
+const form = document.forms[0]
+const insert = document.querySelector('.total');
+
+form.onsubmit = function (e) {
+    e.preventDefault();//отменяет действие по умолчанию... отправку на сервер и перезагрузку страницы
+    insert.innerHTML = Math.sin(form.elements.quantility.value);
+}
