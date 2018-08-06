ab734ec1ddf516785fcc501770c3bec8f7885945
stainlessrat <41853144+stainlessrat@users.noreply.github.com>
2018-08-06 23:12:32 +0700

    bitcoin

---
+++ 01_DOM/home_work_11/js/script.js
@@ -0,0 +1,16 @@
+console.log('You are at ' + window.location);
+
+let crypto = [
+    {
+        name : "Bitcoin",
+        price: 1388.37
+    },
+    {
+        name : "Ethereum",
+        price: 716
+    },
+    {
+        name : "Litecoin",
+        price: 140
+    }
+]
